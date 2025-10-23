5826602d7c96e9b71d6f34fa517beec4fc1994e1
sayeed <expsayeedshorif@gmail.com>
2025-10-23 13:31:34 +0600

    finished with the ui

---
+++ src/App.jsx
@@ -0,0 +1,52 @@
+
+import './App.css'
+import Card from './components/Card'
+import Data from './assets/Data'
+import { useState } from 'react'
+
+function App() {
+  const [score, setScore] = useState(0)
+  const [bestScore, setBestScore] = useState(0)
+  const [array, setArray] = useState(Data)
+
+  function randomArray() {
+    let newData = []
+    let indexes = []
+
+    while (newData.length < 12) {
+      let randomIndex = Math.floor(Math.random() * 12);
+      if (!indexes.includes(randomIndex)) {
+        newData.push(Data[randomIndex])
+        indexes.push(randomIndex)
+      }
+    }
+    return newData;
+  }
+
+  return (
+    <>
+      <div className='min-h-screen py-12 px-8 bg-[#e6e1e1bf]'>
+        <div className='flex items-center justify-between'>
+          <div>
+            <h1 className='text-4xl font-semibold'>Amphibia Memory Game</h1>
+            <p className='text-xl mt-6'>Get points by clicking on an image but don't click on any more than once!</p>
+          </div>
+
+          <div className='text-xl font-semibold'>
+            <p>Score : {score}</p>
+            <p>Best Score  : {bestScore}</p>
+          </div>
+        </div>
+        <div className="card-container grid grid-cols-[repeat(auto-fit,minmax(250px,1fr))] gap-x-16 mt-24 gap-y-12 items-start ">
+          {
+            array.map((elem) => (
+              <Card name={elem.Name} url={elem.url} index={elem.id} />
+            ))
+          }
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
