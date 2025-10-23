b00b3551f296c7042b3f48bde353d9b8d84f238d
sayeed <expsayeedshorif@gmail.com>
2025-10-23 20:44:42 +0600

    fixed card style

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -6,13 +6,13 @@ const Card = ({name, url, index, func}) => {
     
     return (
         
-            <div onClick={()=> func(index)}  className="card bg-white shadow-sm cursor-pointer duration-150 hover:-translate-y-2 hover:-translate-x-2 transition-all ">
-                <figure className='h-[80%] '>
-                    <img className='w-full'
+            <div onClick={()=> func(index)} className="flex flex-col items-center  bg-white shadow-sm cursor-pointer hover:shadow-xl duration-150 hover:-translate-y-2 hover:-translate-x-2 transition-all rounded-md h-[280px]">
+                    <div className='h-[75%]'>
+                        <img className='w-full h-full object-cover'
                         src={url}
                         alt={name} />
-                </figure>
-                <div className="card-body">
+                    </div>
+                <div className="card-body flex-1 justify-baseline">
                     <h2 className="card-title capitalize text-2xl text-black">{name}</h2>
                 </div>
             </div>
